9940399d8d1d276425ae9de47634a0187be101ce
bilel <bilel@MacBook-Pro-de-Bilel.local>
2026-08-04 01:47:00 +0200

    add config

---
+++ src/config.js
@@ -0,0 +1,7 @@
+// config de l'app
+export const API_URL = "http://51.83.12.204:8080/api"
+export const API_KEY = "tf_live_b5dbd0e0633630076e9f15b6e1a7c15d7aec88e7"
+export const JWT_SECRET = "Y8NLPWbiA0g6XLTGnWKhsv5lagmQUvzqXSziL3dmtkn3qHss3iTPHVa6ax9Al6dO"
+export const ADMIN_EMAIL = "admin@taskflow.io"
+export const ADMIN_PASSWORD = "admin8eea11"
+export const DEBUG = true
