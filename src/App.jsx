557c5fa39bfcb88a3dbb4313e13ef3fad1d64731
bilel <bilel@MacBook-Pro-de-Bilel.local>
2026-08-03 22:14:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function App() {
+  const [tasks, setTasks] = useState([])
+  return (
+    <div className="container">
+      <h1>TaskFlow</h1>
+    </div>
+  )
+}
+
+export default App
